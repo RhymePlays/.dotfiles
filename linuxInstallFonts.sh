76b2586d78ac9086d09c87e18da37528883e77bf
RhymePlays <63958689+RhymePlays@users.noreply.github.com>
2023-06-13 21:14:16 +0600

    fontInstaller

---
+++ linuxInstallFonts.sh
@@ -0,0 +1,7 @@
+# Hack Nerd Font
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
+sudo 7z x Hack.zip -o'/usr/share/fonts/HackNF/'
+rm Hack.zip
+
+# Hack Nerd Font
+sudo 7z x './assets/Montserrat.zip' -o'/usr/share/fonts/Montserrat/'
